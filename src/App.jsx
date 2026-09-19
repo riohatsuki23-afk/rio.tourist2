2f23c9513130631c81717a269a445f45de9bcdb4
riohatsuki23-afk <rio.hatsuki23@gmail.com>
2026-09-20 00:59:12 +0800

    Add V1.9 Day 06 illustrated route map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import travelData from "./data/travel-data.json";
+import day06RouteMap from "./assets/maps/day-06-route.png";
 import day07RouteMap from "./assets/maps/day-07-route.png";
 import day08RouteMap from "./assets/maps/day-08-route.png";
 import day09RouteMap from "./assets/maps/day-09-route.png";
 import "./styles/app.css";
 
 const routeMaps = {
+  "day-06-route": day06RouteMap,
   "day-07-route": day07RouteMap,
   "day-08-route": day08RouteMap,
   "day-09-route": day09RouteMap,
